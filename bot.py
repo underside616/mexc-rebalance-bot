bcb1876dfcf33ab6e2a1b19955ae334a8e6ee3b3
underside616 <polkovnikov.victor@gmail.com>
2025-06-26 20:06:34 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -53,4 +53,4 @@ while True:
         rebalance()
     except Exception as e:
         print("❌ Ошибка:", e)
-    time.sleep(300)  # 5 минут
+    time.sleep(10)  # каждый 10 секунд (можно вернуть 300)
